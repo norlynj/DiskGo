87f97741e82a14b3ee5cc3818e9488a6c0f949c3
norlynj <nacastillo2@up.edu.ph>
2023-05-26 21:44:24 +0800

    custom slider and dropdown

---
+++ src/com/view/component/CircleSliderUI.java
@@ -0,0 +1,52 @@
+package view.component;
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicSliderUI;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+public class CircleSliderUI extends BasicSliderUI {
+
+    private Color thumbColor = new Color(219, 220, 222);
+    private Color borderColor = Color.black;
+
+    public CircleSliderUI(JSlider slider) {
+        super(slider);
+        this.thumbColor = thumbColor;
+        this.borderColor = borderColor;
+        slider.setBackground(new Color(231, 205, 194));
+        slider.setFocusable(false); // Disable focus painting
+    }
+
+    protected Dimension getThumbSize() {
+        return new Dimension(16, 16); // Adjust the size as needed
+    }
+
+    @Override
+    public void paintThumb(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        int x = thumbRect.x;
+        int y = thumbRect.y;
+        int width = thumbRect.width;
+        int height = thumbRect.height;
+        int borderSize = 2; // Adjust the border size as needed
+
+        // Draw the border
+        g2d.setColor(borderColor);
+        g2d.fillOval(x - borderSize, y - borderSize, width + borderSize * 2, height + borderSize * 2);
+
+        // Draw the circle thumb
+        g2d.setColor(thumbColor);
+        g2d.fillOval(x, y, width, height);
+    }
+
+    @Override
+    public void paintTrack(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setPaint(Color.GRAY); // Customize track color
+        g2d.setStroke(new BasicStroke(4)); // Customize track thickness
+        g2d.drawLine(trackRect.x, trackRect.y + trackRect.height / 2,
+                trackRect.x + trackRect.width, trackRect.y + trackRect.height / 2);
+    }
+}
---
+++ src/com/view/component/CustomDropDown.java
@@ -0,0 +1,121 @@
+package view.component;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicArrowButton;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+import javax.swing.plaf.basic.BasicComboPopup;
+import javax.swing.plaf.basic.ComboPopup;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class CustomDropDown extends JComboBox<String> {
+
+    private Color backgroundColor = new Color(115, 200, 177);
+    private Color highlightColor = new Color(187, 255, 255);
+    private int cornerRadius = 15;
+
+    public CustomDropDown(String[] items) {
+        super(items);
+        setUI(new CustomDropDownUI());
+        setRenderer(new CustomDropDownRenderer());
+        setOpaque(false);
+        setFocusable(false);
+        setBackground(backgroundColor);
+        setFont(new Font("Montserrat", Font.BOLD, 18));
+    }
+
+    private class CustomDropDownUI extends BasicComboBoxUI {
+
+        @Override
+        protected JButton createArrowButton() {
+            return new ArrowButton(BasicArrowButton.SOUTH);
+        }
+
+        @Override
+        protected ComboPopup createPopup() {
+            return new CustomComboPopup(comboBox);
+        }
+
+        @Override
+        public void paint(Graphics g, JComponent c) {
+            Graphics2D g2d = (Graphics2D) g.create();
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            int width = c.getWidth();
+            int height = c.getHeight();
+
+            int arc = 2 * cornerRadius; // Calculate the arc diameter
+
+            Shape shape = new RoundRectangle2D.Double(0, 0, width - 1, height - 1, arc, arc);
+            g2d.setColor(backgroundColor);
+            g2d.fill(shape);
+
+            // Adjust the display rectangle
+            Rectangle displayRect = rectangleForCurrentValue();
+            displayRect.x += cornerRadius;
+            displayRect.width -= cornerRadius + arrowButton.getWidth();
+
+            // Paint the text
+            g2d.setColor(comboBox.isEnabled() ? comboBox.getForeground() : Color.GRAY);
+            FontMetrics fm = g2d.getFontMetrics();
+            Object selectedItem = comboBox.getSelectedItem();
+            if (selectedItem != null) {
+                String displayText = selectedItem.toString();
+                int textX = displayRect.x + 5;
+                int textY = displayRect.y + ((displayRect.height - fm.getHeight()) / 2) + fm.getAscent();
+                g2d.drawString(displayText, textX, textY);
+            }
+
+            g2d.dispose();
+        }
+    }
+
+    private class CustomComboPopup extends BasicComboPopup {
+
+        public CustomComboPopup(JComboBox combo) {
+            super(combo);
+            setOpaque(true);
+            setBackground(backgroundColor);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, getHeight(), getHeight());
+        }
+    }
+
+    private class CustomDropDownRenderer extends DefaultListCellRenderer {
+
+        @Override
+        public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+            super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            setHorizontalAlignment(SwingConstants.CENTER);
+            setBackground(isSelected ? highlightColor : backgroundColor);
+            setFont(new Font("Montserrat", Font.BOLD, 14));
+            return this;
+        }
+    }
+
+    private class ArrowButton extends BasicArrowButton {
+        public ArrowButton(int direction) {
+            super(direction, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE);
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            // Override the paint method to draw the arrow without a background
+            Dimension size = getSize();
+            Graphics2D g2d = (Graphics2D) g.create();
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setColor(Color.WHITE);
+            int x = (size.width - 4) / 2;
+            int y = (size.height - 8) / 2;
+            int w = 10;
+            int h = 10;
+            g2d.fillPolygon(new int[]{x, x + w, x + (w / 2)}, new int[]{y, y, y + h}, 3);
+            g2d.dispose();
+        }
+    }
+}
