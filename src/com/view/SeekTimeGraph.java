1a52d54e023a1fa6e3e70051b10309e80ca9528a
norlynj <nacastillo2@up.edu.ph>
2023-05-27 00:59:13 +0800

    change ovals to arrows

--- src/com/view/SeekTimeGraph.java
+++ src/com/view/SeekTimeGraph.java
@@ -52,6 +52,10 @@ public class SeekTimeGraph extends JPanel {
         float verticalStep = (float) h / (float) (queue.length + 2);
         float horizontalStep = (float)(w - circleSize) / (float) cylinders;
 
+        // Enable anti-aliasing
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
         g.setColor(bgColor);
 
         g.drawLine(0, 25, w, 25);
@@ -82,7 +86,17 @@ public class SeekTimeGraph extends JPanel {
 
     private void drawArrow(Graphics g, int startX, int startY, int endX, int endY) {
         // Calculate the arrowhead size
-        int arrowSize = 8;
+        int arrowSize = 10;
+
+        // Enable anti-aliasing
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Save the original stroke
+        Stroke originalStroke = g2d.getStroke();
+
+        // Draw the line
+        g.drawLine(startX, startY, endX, endY);
 
         // Calculate the angle of the arrow
         double angle = Math.atan2(endY - startY, endX - startX);
@@ -93,12 +107,17 @@ public class SeekTimeGraph extends JPanel {
         int x2 = (int) (endX - arrowSize * Math.cos(angle + Math.PI / 6));
         int y2 = (int) (endY - arrowSize * Math.sin(angle + Math.PI / 6));
 
-        // Draw the line
-        g.drawLine(startX, startY, endX, endY);
+        // Adjust the thickness of the arrow
+        float arrowThickness = 2.0f;
+        g2d.setStroke(new BasicStroke(arrowThickness));
 
         // Draw the arrowhead
         g.drawLine(endX, endY, x1, y1);
         g.drawLine(endX, endY, x2, y2);
+
+        // Restore the original stroke
+        g2d.setStroke(originalStroke);
+
     }
 
     public int getInitialPointer() {
