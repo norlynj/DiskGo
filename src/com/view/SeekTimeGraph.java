016f971eb59214ef2863328704a9b25182787d1c
norlynj <nacastillo2@up.edu.ph>
2023-06-03 10:28:29 +0800

    put margins on each panel

--- src/com/view/SeekTimeGraph.java
+++ src/com/view/SeekTimeGraph.java
@@ -39,11 +39,13 @@ public class SeekTimeGraph extends JPanel {
                     panel.getRunButton().setVisible(true);
                     panel.getPauseButton().setVisible(false);
                 } else {
-                    System.out.println(totalSeekTime);
+//                    System.out.println(totalSeekTime);
                     totalSeekTime += Math.abs(queue[currentIndex] - queue[currentIndex+1]);
-                    System.out.println(queue[currentIndex] + "-" + queue[currentIndex+1]);
+//                    System.out.println(queue[currentIndex] + "-" + queue[currentIndex+1]);
                     if (!simulateAll) {
                         panel.getTotalSeekTimeLabel().setText(String.valueOf(totalSeekTime));
+                    } else {
+                        panel.getTotalSeekTimeLabel().setText("");
                     }
                     panel.getGraphLabels()[simulatorNumber].setText(panel.getGraphTitles()[simulatorNumber] + " | Total Seek Time: " + totalSeekTime);
                     // Increment the current index
@@ -73,44 +75,44 @@ public class SeekTimeGraph extends JPanel {
         int h = getHeight();
         int w = getWidth();
 
-        float verticalStep = (float) h / (float) (queue.length + 2);
-        float horizontalStep = (float) (w - circleSize) / (float) cylinders;
+        int margin = 30; // Set the margin size
+
+        float verticalStep = (float) (h - 2 * margin) / (float) (queue.length + 2);
+        float horizontalStep = (float) (w - circleSize - 2 * margin) / (float) cylinders;
 
         // Enable anti-aliasing
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
         for (int i = 0; i < queue.length; i++) {
-
             // Draw the string above the line
             FontMetrics fontMetrics = g.getFontMetrics();
             int stringWidth = fontMetrics.stringWidth(String.valueOf(queue[i]));
-            int stringX = (int) (queue[i] * horizontalStep) + (circleSize / 2) - (stringWidth / 2);
-            int stringY = 10; // Adjust the vertical position of the string, e.g., 10 pixels above the line
+            int stringX = (int) (queue[i] * horizontalStep) + (circleSize / 2) - (stringWidth / 2) + margin;
+            int stringY = margin + 10; // Adjust the vertical position of the string, e.g., 10 pixels above the line
             g.drawString(String.valueOf(queue[i]), stringX, stringY);
-            g.drawLine((int) (queue[i] * horizontalStep) + (circleSize / 2), 15, (int) (queue[i] * horizontalStep) + (circleSize / 2), 25);
+            g.drawLine((int) (queue[i] * horizontalStep) + (circleSize / 2) + margin, margin + 15, (int) (queue[i] * horizontalStep) + (circleSize / 2) + margin, margin + 25);
         }
 
         g.setColor(bgColor);
-        g.drawLine(0, 25, w, 25);
+        g.drawLine(margin, margin + 25, w - margin, margin + 25);
 
-        g.drawString(String.valueOf(initialPointer), (int) (initialPointer * horizontalStep) + circleSize, 20);
+        g.drawString(String.valueOf(initialPointer), (int) (initialPointer * horizontalStep) + circleSize + margin, margin + 20);
 
         // Draw arrow from initial pointer to first queue element
-        int startX = (int) (initialPointer * horizontalStep) + (circleSize / 2);
-        int startY = 25;
-        int endX = (int) (queue[0] * horizontalStep) + (circleSize / 2);
-        int endY = (int) (2 * verticalStep);
+        int startX = (int) (initialPointer * horizontalStep) + (circleSize / 2) + margin;
+        int startY = margin + 25;
+        int endX = (int) (queue[0] * horizontalStep) + (circleSize / 2) + margin;
+        int endY = (int) (2 * verticalStep) + margin;
 
         g.drawLine(startX, startY, endX, endY);
         g.fillOval(startX - (circleSize / 2), startY - (circleSize / 2), circleSize, circleSize);
 
         for (int i = 0; i < currentIndex; i++) {
             // Draw only up to the current index
-            startX = (int) (queue[i] * horizontalStep) + (circleSize / 2);
-            startY = (int) ((i + 2) * verticalStep);
-            endX = (int) (queue[i + 1] * horizontalStep) + (circleSize / 2);
-            endY = (int) ((i + 3) * verticalStep);
+            startX = (int) (queue[i] * horizontalStep) + (circleSize / 2) + margin;
+            startY = (int) ((i + 2) * verticalStep) + margin;
+            endX = (int) (queue[i + 1] * horizontalStep) + (circleSize / 2) + margin;
+            endY = (int) ((i + 3) * verticalStep) + margin;
 
             // Calculate the center point of the line
             int centerX = (startX + endX) / 2;
@@ -120,19 +122,20 @@ public class SeekTimeGraph extends JPanel {
             double angle = Math.atan2(endY - startY, endX - startX);
 
             // Coordinates
-            g.drawString(String.valueOf(queue[i]), (int) (queue[i] * horizontalStep) + circleSize, (int) ((i + 1.5) * verticalStep));
+            g.drawString(String.valueOf(queue[i]), (int) (queue[i] * horizontalStep) + circleSize + margin, (int) ((i + 1.5) * verticalStep) + margin);
 
             // Draw the line and circle
             g.drawLine(startX, startY, endX, endY);
             g.fillOval(startX - (circleSize / 2), startY - (circleSize / 2), circleSize, circleSize);
         }
         int lastIndex = currentIndex;
-        int lastX = (int) (queue[lastIndex] * horizontalStep) + (circleSize / 2);
-        int lastY = (int) ((lastIndex + 2) * verticalStep);
+        int lastX = (int) (queue[lastIndex] * horizontalStep) + (circleSize / 2) + margin;
+        int lastY = (int) ((lastIndex + 2) * verticalStep) + margin;
         // Coordinates
-        g.drawString(String.valueOf(queue[lastIndex]), (int) (queue[lastIndex] * horizontalStep) + circleSize, (int) ((lastIndex + 1.5) * verticalStep));
+        g.drawString(String.valueOf(queue[lastIndex]), (int) (queue[lastIndex] * horizontalStep) + circleSize + margin, (int) ((lastIndex + 1.5) * verticalStep) + margin);
         g.fillOval(lastX - (circleSize / 2), lastY - (circleSize / 2), circleSize, circleSize);
     }
+
 
     public int getInitialPointer() {
         return initialPointer;
@@ -149,6 +152,11 @@ public class SeekTimeGraph extends JPanel {
 
     public void setQueue(int[] queue) {
         this.queue = queue;
+        for (int i = 0; i < queue.length; i++){
+            System.out.println(queue[i]);
+        }
+        System.out.println("new queue");
+
         totalSeekTime = Math.abs(initialPointer - queue[0]);
     }
 
